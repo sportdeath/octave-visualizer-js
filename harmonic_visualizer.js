3b5cf8dd4f3b412d9c95252392f473265397a748
tfh <tfh@mit.edu>
2020-12-05 14:26:37 -0500

    Harmonic visualizer class

--- harmonic_visualizer.js
+++ harmonic_visualizer.js
@@ -1,14 +1,17 @@
-// Initialize the color wheel
-// and a place for it's values
-var cw = new ColorWheel();
-let cw_values = new Array(NUM_SLICES);
-
-// Run the animation
-function animate() {
-  for (i = 0; i < NUM_SLICES; i++) {
-    cw_values[i] = 0.8 + 0.1 * Math.random();
-  }
-  cw.draw(cw_values);
-  requestAnimationFrame(animate);
+function HarmonicVisualizer() {
+  this.cw = new ColorWheel();
+  this.cw_values = new Array(NUM_SLICES);
+  this.getMicrophoneInput();
+  this.animate();
 }
-animate();
+
+HarmonicVisualizer.prototype.getMicrophoneInput = function() {
+}
+
+HarmonicVisualizer.prototype.animate = function() {
+  for (i = 0; i < NUM_SLICES; i++) {
+    this.cw_values[i] = 0.8 + 0.1 * Math.random();
+  }
+  this.cw.draw(this.cw_values);
+  requestAnimationFrame(this.animate.bind(this));
+}
