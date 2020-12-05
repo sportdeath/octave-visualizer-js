0ac5cd37723b1e31c469e07987ae71ab89a7b3af
tfh <tfh@mit.edu>
2020-12-05 15:32:00 -0500

    Computes volume ,,, fft doesn't give complex :'(

--- harmonic_visualizer.js
+++ harmonic_visualizer.js
@@ -14,17 +14,35 @@ HarmonicVisualizer.prototype.getMicrophoneInput = function() {
 }
 
 HarmonicVisualizer.prototype.onStream = function(stream) {
-  var input = this.ctx.createMediaStreamSource(stream);
-  var analyser = this.ctx.createAnalyser();
+  // Fetch the input and connect it to the fft
+  this.audio_in = this.ctx.createMediaStreamSource(stream);
+  this.analyser = this.ctx.createAnalyser();
+  this.analyser.fftSize = 256;
+  this.audio_in.connect(this.analyser);
+
+  // Create storage for data
+  this.audio_data = new Float32Array(this.analyser.frequencyBinCount);
 
   // Animate!
   this.animate();
 }
 
 HarmonicVisualizer.prototype.animate = function() {
-  for (i = 0; i < NUM_SLICES; i++) {
-    this.cw_values[i] = 0.8 + 0.1 * Math.random();
+  // Fetch the fft data!
+  this.analyser.getFloatFrequencyData(this.audio_data);
+
+  var total_volume = 0;
+  for (var i = 0; i < this.analyser.frequencyBinCount; i++) {
+    this.audio_data[i] -= this.analyser.minDecibels;
+    this.audio_data[i] /= this.analyser.maxDecibels - this.analyser.minDecibels;
+    total_volume += this.audio_data[i];
   }
+  var avg_volume = total_volume/(this.analyser.frequencyBinCount);
+
+  for (var i = 0; i < NUM_SLICES; i++) {
+    this.cw_values[i] = avg_volume;
+  }
+
   this.cw.draw(this.cw_values);
   requestAnimationFrame(this.animate.bind(this));
 }
