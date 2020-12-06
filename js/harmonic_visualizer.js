93f6e104fe0a62299351f6e19e61de185362aebd
tfh <tfh@mit.edu>
2020-12-05 22:00:03 -0500

    audio vector into c++

--- js/harmonic_visualizer.js
+++ js/harmonic_visualizer.js
@@ -1,3 +1,5 @@
+const FFT_SIZE = 1024;
+
 function HarmonicVisualizer() {
   // Initialize the color wheel
   this.cw = new ColorWheel();
@@ -17,15 +19,16 @@ HarmonicVisualizer.prototype.onStream = function(stream) {
   // Fetch the input and connect it to the fft
   this.audio_in = this.ctx.createMediaStreamSource(stream);
   this.analyser = this.ctx.createAnalyser();
-  this.analyser.fftSize = 256;
+  this.analyser.fftSize = FFT_SIZE;
   this.audio_in.connect(this.analyser);
 
   // Create storage for data
-  this.audio_data = new Float32Array(this.analyser.frequencyBinCount);
+  this.audio_data = new Float32Array(FFT_SIZE);
+  this.audio_data_vec = new Module.VectorFloat();
+  this.audio_data_vec.resize(FFT_SIZE, 0);
 
   // Initialize the object!
-  this.octave = new Module.Octave(this.analyser.fftSize, NUM_SLICES);
-  alert(this.octave.get_audio_size());
+  this.octave = new Module.Octave(FFT_SIZE, NUM_SLICES);
 
   // Animate!
   this.animate();
@@ -35,17 +38,17 @@ HarmonicVisualizer.prototype.animate = function() {
   // Fetch the time series
   this.analyser.getFloatTimeDomainData(this.audio_data);
 
-  // TODO: do some computation with C++/Emscripten
-  var total_volume = 0;
-  for (var i = 0; i < this.analyser.frequencyBinCount; i++) {
-    total_volume += Math.abs(this.audio_data[i]);
+  // Copy it into a vector
+  for (var i = 0; i < this.audio_data.length; i++) {
+    this.audio_data_vec.set(i, this.audio_data[i]);
   }
-  var avg_volume = total_volume/(this.analyser.frequencyBinCount);
-  avg_volume = Math.sqrt(avg_volume);
+
+  // Extract the harmonic components with C++
+  var slices = this.octave.audioToSlices(this.audio_data_vec);
 
   // Color the wheel
   for (var i = 0; i < NUM_SLICES; i++) {
-    this.cw_values[i] = avg_volume;
+    this.cw_values[i] = slices[i];
   }
   this.cw.draw(this.cw_values);
   requestAnimationFrame(this.animate.bind(this));
--- src/octave.cpp
+++ src/octave.cpp
@@ -1,24 +1,36 @@
+#include <iostream>
+#include <vector>
+
 #include <emscripten/bind.h>
 using namespace emscripten;
 
 class Octave {
   public:
-    Octave(int audio_size_, int num_bins_) {
-      audio_size = audio_size_;
+    Octave(int audio_size, int num_bins) {
+      slices = std::vector<float>(num_bins);
+      window = std::vector<float>(audio_size);
+      window_d = std::vector<float>(audio_size);
     }
 
-    int get_audio_size() {
-      return audio_size;
+    std::vector<float> & audio_to_slices(std::vector<float> & audio) {
+      for (size_t i = 0; i < slices.size(); i++) {
+        slices[i] = audio[i] * 2;
+      }
+
+      return slices;
     }
 
   private:
-    int audio_size;
+    std::vector<float> slices;
+    std::vector<float> window;
+    std::vector<float> window_d;
 };
 
 // Binding code
 EMSCRIPTEN_BINDINGS(octave) {
   class_<Octave>("Octave")
   .constructor<int, int>()
-  .function("get_audio_size", &Octave::get_audio_size)
+  .function("audioToSlices", &Octave::audio_to_slices)
   ;
+  register_vector<float>("VectorFloat");
 }
