fd1539889516ce4e1fc8c3db26b870b480eba1ed
tfh <tfh@mit.edu>
2020-12-09 22:24:48 -0500

    precomputation

--- js/octave.js
+++ js/octave.js
@@ -10,6 +10,8 @@ function Octave(audioSize, numBins, sampleRate) {
   this.audio    = new Float32Array(audioSize);
   this.hann     = new Float32Array(audioSize);
   this.hannD    = new Float32Array(audioSize);
+  this.cosTable = new Float32Array(audioSize/2);
+  this.sinTable = new Float32Array(audioSize/2);
 
   // Precompute the Hann windows
   for (var i = 0; i < audioSize; i++) { 
@@ -18,6 +20,12 @@ function Octave(audioSize, numBins, sampleRate) {
     var angle = (2 * Math.PI * n)/(N - 1);
     this.hann[i] = 0.5 * (1 + Math.cos(angle));
     this.hannD[i] = -(Math.PI * this.sampleRate)/(N - 1) * Math.sin(angle);
+  }
+
+  // Precompute trig table
+  for (var i = 0; i < audioSize/2; i++) {
+    this.cosTable[i] = Math.cos(2 * Math.PI * i / audioSize);
+    this.sinTable[i] = Math.sin(2 * Math.PI * i / audioSize);
   }
 }
 
@@ -34,22 +42,23 @@ Octave.prototype.processAudio = function() {
   this.windowDI.fill(0);
   
   // Compute the FFT
-  FFT(this.windowR, this.windowI);
-  FFT(this.windowDR, this.windowDI);
+  this.fft(this.windowR, this.windowI);
+  this.fft(this.windowDR, this.windowDI);
 
   // Wrap it to the octave
   for (var i = 1; i < this.windowR.length/2 + 1; i++) {
-    // Precompute the norm
+    // Compute the norm
     var norm = this.windowR[i] * this.windowR[i] + this.windowI[i] * this.windowI[i]; 
 
-    // Compute the frequency
+    // Compute the reassigned frequency
     var freq = (2 * Math.PI * i * this.sampleRate)/(this.windowR.length);
     var dPhaseDT = (this.windowI[i] * this.windowDR[i] - this.windowR[i] * this.windowDI[i])/norm;
     var freqReassigned = freq + dPhaseDT;
     var wrappedFreq = Math.log2(freqReassigned) % 1;
 
-    // Find the bin
-    this.placeSlice(wrappedFreq, Math.sqrt(norm));
+    // Place it in a bin
+    var value = Math.sqrt(norm);
+    this.placeSlice(wrappedFreq, value);
   }
 }
 
@@ -61,29 +70,11 @@ Octave.prototype.placeSlice = function(position, value) {
   this.slices[(leftOfBin + 1) % this.slices.length] += rightPercent * value;
 }
 
-function FFT(real, imag) {
+Octave.prototype.fft = function(real, imag) {
  // https://www.nayuki.io/res/free-small-fft-in-multiple-languages/fft.js
  // Length variables
  var n = real.length;
- if (n != imag.length)
-  throw "Mismatched lengths";
- if (n == 1)  // Trivial transform
-  return;
- var levels = -1;
- for (var i = 0; i < 32; i++) {
-  if (1 << i == n)
-   levels = i;  // Equal to log2(n)
- }
- if (levels == -1)
-  throw "Length is not a power of 2";
-
- // Trigonometric tables
- var cosTable = new Array(n / 2);
- var sinTable = new Array(n / 2);
- for (var i = 0; i < n / 2; i++) {
-  cosTable[i] = Math.cos(2 * Math.PI * i / n);
-  sinTable[i] = Math.sin(2 * Math.PI * i / n);
- }
+ var levels = Math.log2(n);
 
  // Bit-reversed addressing permutation
  for (var i = 0; i < n; i++) {
@@ -105,8 +96,8 @@ function FFT(real, imag) {
   for (var i = 0; i < n; i += size) {
    for (var j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
     var l = j + halfsize;
-    var tpre =  real[l] * cosTable[k] + imag[l] * sinTable[k];
-    var tpim = -real[l] * sinTable[k] + imag[l] * cosTable[k];
+    var tpre =  real[l] * this.cosTable[k] + imag[l] * this.sinTable[k];
+    var tpim = -real[l] * this.sinTable[k] + imag[l] * this.cosTable[k];
     real[l] = real[j] - tpre;
     imag[l] = imag[j] - tpim;
     real[j] += tpre;
