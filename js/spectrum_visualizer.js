27d5518fbf32e5b9e53750ea05dc1128d6b8ae9b
theia <theia@mit.edu>
2022-01-07 14:05:43 -0500

    The spectrum is now vertical and symmetrical

--- js/spectrum_visualizer.js
+++ js/spectrum_visualizer.js
@@ -4,26 +4,35 @@ export default class SpectrumVisualizer extends BaseVisualizer {
 
   constructor(elementSelector, numPeaks, normalizationTC) {
     super(elementSelector, numPeaks, normalizationTC)
-    this.circles = new Array(numPeaks)
+    this.rects = new Array(numPeaks)
     for (var i = 0; i < this.numPeaks; i++) {
-      this.circles[i] = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-      this.circles[i].setAttribute('fill', 'red')
-      this.circles[i].setAttribute('width', '0.2%')
-      this.circles[i].setAttribute('y', '0%')
-      this.svg.appendChild(this.circles[i])
+      this.rects[i] = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+      this.rects[i].setAttribute('fill', 'red')
+      this.rects[i].setAttribute('fill-opacity', '0.4')
+      this.rects[i].setAttribute('height', '0.5%')
+      this.svg.appendChild(this.rects[i])
     }
   }
 
   draw(freq, value) {
-    var maxFreq = Math.log2(freq[this.numPeaks-1])
+    // Human hearing range, roughly
+    var freqMinLog = Math.log2(20)
+    var freqMaxLog = Math.log2(20000)
+
     for (var i = 0; i < this.numPeaks; i++) {
-      var x = Math.log2(freq[i])/maxFreq
+
+      var x = (Math.log2(freq[i]) - freqMinLog)/(freqMaxLog - freqMinLog)
+      if (x < 0 || x > 1) continue
+
       var y = value[i]
+
+      // TODO: why are there NaNs in the first place...
       if (!isNaN(x) && !isNaN(y)) {
-        this.circles[i].setAttribute('x', `${100*x}%`)
-        this.circles[i].setAttribute('height', `${100*y}%`)
+        this.rects[i].setAttribute('y', `${100*(1 - x)}%`)
+        this.rects[i].setAttribute('width', `${100*y}%`)
+        this.rects[i].setAttribute('x', `${50*(1-y)}%`)
       }
+
     }
   }
-
 }
